e0f0c54ef011990cb80cd08c6032621e8e6bc370
spe <peg@primase.fr>
2024-04-16 00:06:56 +0200

    Démo notification

--- app/src/main/java/fr/nextu/guerton_pierreemmanuel/MainActivity2.kt
+++ app/src/main/java/fr/nextu/guerton_pierreemmanuel/MainActivity2.kt
@@ -1,11 +1,21 @@
 package fr.nextu.guerton_pierreemmanuel
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -50,7 +60,7 @@ class MainActivity2 : AppCompatActivity() {
             AppDatabase::class.java, "movies.db"
         ).build()
 
-
+        createNotificationChannel()
     }
 
     override fun onStart() {
@@ -74,6 +84,7 @@ class MainActivity2 : AppCompatActivity() {
         }
     }
 
+
     fun requestMoviesList() = CoroutineScope(Dispatchers.IO).async {
         val client = OkHttpClient()
 
@@ -85,12 +96,59 @@ class MainActivity2 : AppCompatActivity() {
             .build()
 
         val response: Response = client.newCall(request).execute()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            notifyNewData(response)
+        }
         moviesFromJson(response.body?.string() ?: "")
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun notifyNewData(response: Response) {
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(getString(R.string.movies_updated_title))
+            .setContentText(response.body?.string() ?: "")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    this@MainActivity2,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1)
+                return@with
+            }
+            notify(1, builder.build())
+        }
     }
 
     fun moviesFromJson(json: String) {
         val gson = Gson()
         val om = gson.fromJson(json,  Movies::class.java)
         db.movieDao().insertAll(*om.movies.toTypedArray())
+    }
+
+
+    companion object {
+        const val CHANNEL_ID = "fr_nextu_guerton_pierreemmanuel_channel_notification"
     }
 }
