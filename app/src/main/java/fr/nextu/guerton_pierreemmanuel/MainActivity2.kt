956ca9184a2e88bd165a68a900a6279c69438e63
spe <peg@primase.fr>
2024-04-15 19:11:55 +0200

    Adding custom back button

--- app/src/main/java/fr/nextu/guerton_pierreemmanuel/MainActivity2.kt
+++ app/src/main/java/fr/nextu/guerton_pierreemmanuel/MainActivity2.kt
@@ -1,6 +1,9 @@
 package fr.nextu.guerton_pierreemmanuel
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,11 @@ class MainActivity2 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<ImageButton>(R.id.back).setOnClickListener {
+            //startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
